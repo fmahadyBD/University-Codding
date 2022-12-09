117a4bb970fe3d8a8458dc995c43b5f4e6b1cec2
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-12-09 15:20:44 +0600

    Add files via upload

--- CSE 105 Assingment Graph Traversal/BSF.c
+++ CSE 105 Assingment Graph Traversal/BSF.c
@@ -1,35 +1,54 @@
-#include<stdio.h>
-int a[20][20],q[20],visited[20],n;
+#include <stdio.h>
+int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
 
-void dfs(int v){
-    int i;
-    for(i=0;i<n;i++){//check the vertex in the graph
-        if(a[v][i]!=0 && visited[i]==0){// adjacence to v and not visited 
-            visited[i]=1;// make the vertex visited
-            printf("%d \t",i);
-            dfs(i);
+void bsf(int v)
+{
+    for (i = 0; i < n; i++)
+    {
+        if (a[v][i] && !visited[i])
+        {
+
+            q[++r] = i;
+        }
+        if (f <= r)
+        {
+            visited[q[f]] = 1;
+            bsf(q[++f]);
         }
     }
 }
 
-
-int main(){
-    int v,i,j;
-    printf("Enter the number of vertices:\n");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        visited[i]=0;// make all visited is 0
-    }
+int main()
+{
+    int v;
+    printf("Enter the number of verticies:\n");
+    scanf("%d", &n);
     printf("Enter the graph data matrix:\n");
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            scanf("%d",&a[i][j]);// input the graph.
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]); // input the graph.
         }
     }
-printf("Enter the starting vertex\n");
-scanf("%d",&v);
-printf("The DFS Traversal is\n");
-visited[v]=1;// make the vertex is visited
+    printf("Enter the starting vertex\n");
+    scanf("%d", &v);
 
-dfs(v);
+    for (i = 0; i < n; i++)
+    {
+
+        q[i] = 0;
+        visited[i] = 0; // make all visited is 0
+    }
+    bsf(v);
+ 
+    printf("The reachebal node are:\n");
+    for (i = 0; i < n; i++)
+    {
+        if (visited[i])
+        {
+            printf("%d\t", i);
+        }
+    }
 }
+
