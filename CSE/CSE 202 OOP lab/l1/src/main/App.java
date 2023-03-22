b9a11f270ab0b24dabc815ce0170ecc74ed49c13
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-03-22 21:41:18 +0600

    Add files via upload

---
+++ CSE/CSE 202 OOP lab/l1/src/main/App.java
@@ -0,0 +1,12 @@
+package main;
+
+import worker.Programmer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+      Programmer obj=new Programmer(0, null, 0, 0, 0);
+      obj.assingcom();
+        
+    }
+}
---
+++ CSE/CSE 202 OOP lab/l1/src/worker/OfficeEMpo.java
@@ -0,0 +1,10 @@
+package worker;
+
+public class OfficeEMpo extends Employee{
+
+    OfficeEMpo(int id, String name, int age) {
+        super(id, name, age);
+        
+    }
+    
+}
