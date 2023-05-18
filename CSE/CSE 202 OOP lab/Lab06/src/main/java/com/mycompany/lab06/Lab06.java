445b77bcc98e49f297f92fa203539a2b42bded8d
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-05-18 14:39:02 +0600

    Add files via upload

---
+++ CSE/CSE 202 OOP lab/Lab06/src/main/java/com/mycompany/lab06/Lab06.java
@@ -0,0 +1,42 @@
+
+package com.mycompany.lab06;
+
+
+public class Lab06 {
+    public static void main(String[] args) {
+        int redW=40;
+        int blueW=50;
+        Red r=new Red();
+        Blue b=new Blue();
+        System.out.println("Red Collide with Blue and wigth: "+r.colidWith(blueW));
+        System.out.println("Blue Collide with Red and wigth: "+b.colidWith(redW));
+    }
+   
+}
+
+
+class Red {
+    
+    int rw=40;
+
+   int colidWith(int b){
+       
+       return b+rw;
+       
+   }
+
+}
+class Blue {
+    int bw=50;
+//    Blue(int rw,int bw){
+//        this.rw=rw;
+//        this.bw=bw;
+//        
+//    }
+  int colidWith(int r){
+       
+       return r+bw;
+       
+   }
+
+}
