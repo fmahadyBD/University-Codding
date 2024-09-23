fb81db6656cb6921691e4e64df09ca3b4135123a
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2024-09-23 12:43:04 +0600

    Upload OS Bash code
    First class

---
+++ CSE/Operating  System/Class one/mahady.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+read -p "Enter one side: " x
+read -p "Enter another side: " y
+
+let ans=x*y
+
+
+
+expr 2 + 2 
+
+echo "Area is $ans"
+read -p "Enter your name: " name
+echo "Hey, $name !"
+
+read -p "Enter a: " a
+read -p "Enter b: " b
+let c=a+b
+
+echo " The Sum $c"
